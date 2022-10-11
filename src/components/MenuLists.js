a172580be343f91c399f6cdcdbf7b257c586818a
rahul2681 <80594958+rahul2681@users.noreply.github.com>
2022-10-11 11:45:07 +0530

    Add files via upload

---
+++ src/components/MenuLists.js
@@ -0,0 +1,39 @@
+import deepwork from "../img/deep_work.jpg";
+import Margherita from "../img/ikigai.jpg";
+import PedroTechSpecial from "../img/mindset.jpg";
+import Vegan from "../img/think_like_a_monk.jpg";
+import Pineapple from "../img/thinking_big.jpg";
+import Expensive from "../img/atomic_habit.jpg";
+
+export const MenuList = [
+  {
+    name: "Deep Work",
+    image: deepwork,
+    price: 15.99,
+  },
+  {
+    name: "Ikigai",
+    image: Margherita,
+    price: 11.99,
+  },
+  {
+    name: "Mindset",
+    image: PedroTechSpecial,
+    price: 256.53,
+  },
+  {
+    name: "Think Like A Monk",
+    image: Vegan,
+    price: 17.99,
+  },
+  {
+    name: "Thinking Big",
+    image: Pineapple,
+    price: 4.99,
+  },
+  {
+    name: "Atomic Habit",
+    image: Expensive,
+    price: 1997.99,
+  },
+];
